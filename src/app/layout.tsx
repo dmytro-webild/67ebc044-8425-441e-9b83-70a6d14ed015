224073f0be1816381a5c222040355a3ac0eeda5e
bender <kudin.dima@gmail.com>
2026-03-25 01:43:45 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ServiceWrapper } from "@/components/ServiceWrapper";
 import Tag from "@/tag/Tag";
 import { getVisualEditScript } from "@/utils/visual-edit-script";
 import { Manrope } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 
 
 
@@ -41,8 +42,13 @@ export const metadata: Metadata = {
   },
 };
 
-const manrope = Manrope({
-  variable: "--font-manrope",
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -54,7 +60,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${manrope.variable} antialiased`}>
+        <body className={`${inter.variable} ${openSans.variable} antialiased`}>
           <Tag />
           {children}
           <script
